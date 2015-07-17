1551462f222fecd1da80e2290ad5650efe0357d7
m5181130 <m518113@u-aizu.ac.jp>
2015-07-17 12:47:21 +0900

    update

--- libraries/Power/savepower.h
+++ libraries/Power/savepower.h
@@ -7,6 +7,22 @@
 #include <avr/power.h>
 #include <avr/wdt.h>
 
+// watchdog intervals
+// sleep bit patterns for WDTCSR
+enum 
+{
+ WDT_16_MS  =  0b000000,
+ WDT_32_MS  =  0b000001,
+ WDT_64_MS  =  0b000010,
+ WDT_128_MS =  0b000011,
+ WDT_256_MS =  0b000100,
+ WDT_512_MS =  0b000101,
+ WDT_1_SEC  =  0b000110,
+ WDT_2_SEC  =  0b000111,
+ WDT_4_SEC  =  0b100000,
+ WDT_8_SEC  =  0b100001,
+};  // end of WDT intervals enum
+
 void setSleepInterrupt(void)
 {
  cli();
