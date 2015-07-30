07754ae67846ccacf877cc7d386c4133b1a10b74
m5181130 <m518113@u-aizu.ac.jp>
2015-07-30 16:00:33 +0900

    update

--- libraries/Power/savepower.h
+++ libraries/Power/savepower.h
@@ -26,20 +26,17 @@ enum
 void setSleepInterrupt(void)
 {
  cli();
- MCUSR &= ~(1<<WDRF);
- WDTCSR |= (1<<WDCE) | (1<<WDE);
- WDTCSR = 1<<WDP0 | 1<<WDP3; /* 8.0 seconds */
- WDTCSR |= _BV(WDIE);
+ wdt_reset();
+ MCUSR &= ~_BV(WDRF);
+ WDTCSR = _BV(WDCE) | _BV(WDE);
+ WDTCSR = _BV(WDIE) | WDT_8_SEC;
  sei();
 }
 
 void enterSleep(void)
 {
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  sleep_enable();
-  sleep_mode();
-  sleep_disable();
-  power_all_enable();
+ set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+ sleep_mode();
 }
 
 ISR(WDT_vect)
