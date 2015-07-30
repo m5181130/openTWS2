1a8fb4dc7ac1fdb8dea503e646033b4869432ae1
m5181130 <m518113@u-aizu.ac.jp>
2015-07-30 18:31:36 +0900

    new

---
+++ libraries/OpenTWS/examples/blinkLED/blinkLED.ino
@@ -0,0 +1,14 @@
+#include "OpenTWS.h"
+
+void setup() {
+  pinMode(led1, OUTPUT);   
+  pinMode(led2, OUTPUT);  
+}
+
+void loop(){
+  digitalWrite(led1, !digitalRead(led1));
+
+  delay(300);
+
+  digitalWrite(led2, !digitalRead(led2));
+}
